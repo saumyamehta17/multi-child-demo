fde957c9134c4a01b46bc749f5cb80d16d36be1e
sweety <smehta1289@gmail.com>
2013-06-04 13:42:45 +0530

    access models in one form

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,11 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name,:task_attributes
   has_many :tasks
 
   def task_attributes=(task_attributes)
     task_attributes.each do |attr|
       self.tasks.build(attr)
-    end 
+    end
   end
   
 end
